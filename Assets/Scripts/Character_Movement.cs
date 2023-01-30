883bc69dd56453addc05c39b453f2a497c4abc4b
60016254 <60016254@nrc.ac.uk>
2023-01-30 16:50:26 +0000

    Cooking Guy Gets Hot

--- Assets/Scripts/Character_Movement.cs
+++ Assets/Scripts/Character_Movement.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Character_movement : MonoBehaviour
+public class Character_Movement : MonoBehaviour
 {
     public CharacterController controller;
 
