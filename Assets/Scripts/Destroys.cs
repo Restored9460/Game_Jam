e9550d0be2b471694777d8e5685421f0366edc27
Restored9460 <123942221+Restored9460@users.noreply.github.com>
2023-02-03 00:28:08 +0000

    renaming and management

--- Assets/Scripts/Destroys.cs
+++ Assets/Scripts/Destroys.cs
@@ -6,22 +6,22 @@ public class Destroys : MonoBehaviour
 {
     void OnCollisionEnter(Collision collision)
     {
-        //Check for a match with the specified name on any GameObject that collides with your GameObject
+        
         if (collision.gameObject.name == "Order")
         {
-            //If the GameObject's name matches the one you suggest, output this message in the console
+            // this makes him stop
             GameObject varGameObject = GameObject.FindWithTag("Customer");
             varGameObject.GetComponent<Navigation>().enabled = false;
-            Debug.Log("HE talking");
+           
         }
 
-        //Check for a match with the specific tag on any GameObject that collides with your GameObject
-        if (collision.gameObject.tag == "emptyBowl")
+        //chane the tag here to make the npc to other food
+        if (collision.gameObject.tag == "PotBeet")
         {
 
             GameObject varGameObject = GameObject.FindWithTag("Customer");
             varGameObject.GetComponent<CustomerKiller>().enabled = true;
-            Debug.Log("Walks away");
+           
         }
     }
 }
