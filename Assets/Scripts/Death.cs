966844dcf982b629c764d1fc00eaff3a7c2507de
Restored9460 <123942221+Restored9460@users.noreply.github.com>
2023-02-03 00:20:50 +0000

    npc reset

--- Assets/Scripts/Death.cs
+++ Assets/Scripts/Death.cs
@@ -4,13 +4,19 @@ using UnityEngine;
 
 public class Death : MonoBehaviour
 {
+    
+
     void OnCollisionEnter(Collision collision)
     {
+        //Check for a match with the specified name on any GameObject that collides with your GameObject
         if (collision.gameObject.name == "Order")
         {
-            Destroy(collision.gameObject);
+            //If the GameObject's name matches the one you suggest, output this message in the console
+            GameObject varGameObject = GameObject.FindWithTag("Customer");
+            varGameObject.GetComponent<Navigation>().enabled = true;
+            varGameObject.GetComponent<CustomerKiller>().enabled = false;
+            Debug.Log("HE talking");
         }
 
     }
-
 }
--- Assets/Scripts/Navigation.cs
+++ Assets/Scripts/Navigation.cs
@@ -4,11 +4,24 @@ using UnityEngine;
 
 public class Navigation : MonoBehaviour
 {
+    public Transform Reset;
+    public GameObject Order;
+
     public Transform target;
     public float step = 1.0f;
+  
     public void Update()
     {
+     
         transform.Translate(Vector3.Normalize(target.position - transform.position) * step);
 
     }
+    void OnCollisionEnter(Collision collision)
+    {
+         if (collision.gameObject.name == "CubeofDeath")
+        {
+            Order.transform.position = Reset.transform.position;
+        }
+    }
+
 }
